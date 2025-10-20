ade5d30350751cc8504db4a4774bf4cc6a48aa0b
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-20 15:39:52 +0200

    Ajout d'un README

--- Boat.py
+++ Boat.py
@@ -1,17 +1,11 @@
+"""
+Classe permettant de définir les informations d'un bateau
+"""
+
 class Boat:
     boats = []
-
-    @classmethod
-    def nb_boats(cls):
-        return len(cls.boats)
-
 
     def __init__(self, position,name):
         self.position = position
         self.name = name
         Boat.boats.append(self)
-
-
-    def __repr__(self):
-        boat_name = self.name
-        return f"{boat_name}{self.position}"
