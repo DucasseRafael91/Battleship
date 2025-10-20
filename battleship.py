0210f8f036930eba77829746ce4ed7aebaf26c74
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-20 11:23:54 +0200

    Correction for pos in boat to for pos in boat.position

--- battleship.py
+++ battleship.py
@@ -13,27 +13,27 @@ def create_ships():
     boats = []
 
     # Porte-avions (B2 à F2) : ligne 1, colonnes 1-5
-    aircraft = Boat([(i, 1) for i in range(1, 6)],"Aircraft")
+    aircraft = Boat([(1, i) for i in range(1, 6)], "Aircraft")
     boats.append(aircraft)
 
-    # Croiseur (A4 à A7)
-    cruiser = Boat([(i, 1) for i in range(1, 6)], "Cruiser")
+    # Croiseur (A4 à A7) : ligne 3, colonnes 0-3
+    cruiser = Boat([(3, i) for i in range(0, 4)], "Cruiser")
     boats.append(cruiser)
 
-    # Contre-torpilleur (C5 à E5)
-    destroyer = Boat([(i, 1) for i in range(1, 6)], "Destroyer")
+    # Contre-torpilleur (C5 à E5) : lignes 2-4, colonne 4
+    destroyer = Boat([(i, 4) for i in range(2, 5)], "Destroyer")
     boats.append(destroyer)
 
-    # Sous-marin (H5 à J5)
-    submarine = Boat([(i, 1) for i in range(1, 6)],"Submarine")
+    # Sous-marin (H5 à J5) : lignes 7-9, colonne 4
+    submarine = Boat([(i, 4) for i in range(7, 10)], "Submarine")
     boats.append(submarine)
 
-    # Torpilleur (E9 à F9)
-    torpedo_boat = Boat([(i, 1) for i in range(1, 6)], "Torpedo Boat")
+    # Torpilleur (E9 à F9) : lignes 4-5, colonne 8
+    torpedo_boat = Boat([(i, 8) for i in range(4, 6)], "Torpedo Boat")
     boats.append(torpedo_boat)
 
-    print(boats)
     return boats
+
 
 def create_empty_grid():
     """
@@ -92,7 +92,7 @@ def main():
 
     boats_position = []
     for boat in boats:
-        for pos in boat:
+        for pos in boat.position:
             if pos not in boats_position:
                 boats_position.append(pos)
 
@@ -120,7 +120,7 @@ def main():
 
             # Vérifier si un navire est coulé
             for index, boat in enumerate(boats):
-                if all(pos in hit_positions for pos in boat) and index not in sink_boats:
+                if all(pos in hit_positions for pos in boat.position) and index not in sink_boats:
                     print(f"\n {boats_names[index]} COULÉ ! \n")
                     sink_boats.append(index)
 
